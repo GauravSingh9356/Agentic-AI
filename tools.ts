47d15c559220c367966d2b01a31b8a5a6add4f1b
Gaurav Singh <gauravsingh9356@Gauravs-MacBook-Air.local>
2026-01-03 21:34:35 +0530

    Setup the tools  - create event, get event tool

---
+++ tools.ts
@@ -0,0 +1,35 @@
+import { tool } from "@langchain/core/tools";
+import z from "zod";
+
+export const createEventTool = tool(
+  async function ({ input: string }) {
+    return "The meeting has been scheduled.";
+  },
+  {
+    name: "create-event",
+    description: "Call to create a calendar event",
+    schema: z.object({}),
+  }
+);
+
+export const getEventsTool = tool(
+  async function ({ input: string }) {
+    return JSON.stringify([
+      {
+        title: "Team Meeting",
+        date: "2024-07-01",
+        time: "10:00 AM",
+      },
+      {
+        title: "Doctor Appointment",
+        date: "2024-07-02",
+        time: "3:00 PM",
+      },
+    ]);
+  },
+  {
+    name: "get-events",
+    description: "Call to get the calendar events",
+    schema: z.object({}),
+  }
+);
